c234a89fde9ea847456efa3f6eb1c7779b9ae677
mkirilyuk <mkirilyuk@27co.de>
2023-04-07 23:02:30 +0300

    init

--- base.py
+++ base.py
@@ -1,15 +1,20 @@
 import glob
 import os
+from typing import Union
 
 import pandas as pd
 from tqdm.notebook import tqdm
 
 
 class BaseDataFrame:
-    def __init__(self, data_path):
+    def __init__(self, data_path: Union[str, os.PathLike]):
         csv_paths = glob.glob(os.path.join(data_path, "**", "*.csv"))
         self.data = {os.path.basename(path).split('.')[0]: pd.read_csv(path, sep=';') for path in tqdm(csv_paths)}
         desc = pd.read_excel(glob.glob(os.path.join(data_path, "*.xlsx"))[0], sheet_name=None)
         desc.pop('Перечень файлов')
         self.desc = {key.split('.')[1]: desc[key] for key in desc}
         assert self.data.keys() == self.desc.keys(), "Keys must be equal"
+        assert len(self.data.keys()) == 26, "Keys length must equals 26"
+
+
+data = BaseDataFrame('data')
